9564b4a29b36e362e3c9a7731be43914b26a1c4b
pavithran <pavithran050701@gmail.com>
2022-03-10 11:04:39 +0530

    Fixed

--- src/main/java/powerpackage/powerfinder.java
+++ src/main/java/powerpackage/powerfinder.java
@@ -1,11 +1,12 @@
 package powerpackage;
 
-public class powerfinder {
-    public static int OF(int xyz, int pqr) {
-        int p = 1;
-        for (int i = 0; i<pqr; i++) {
-            p *= xyz;
-        }
-        return p;
+public class PowerFinder {
+
+  public static int OF(int xyz, int pqr) {
+    int p = 1;
+    for (int i = 0; i < pqr; i++) {
+      p *= xyz;
     }
+    return p;
+  }
 }
